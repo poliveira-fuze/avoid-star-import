8cbf0d802289d69a18c90c6ce0cb4e39e772a17a
Paulo Oliveira <paulo.oliveira@8x8.com>
2024-09-13 13:54:30 +0100

    new-code: expand star imports

--- src/main/java/com/github/poliveira_fuze/AvoidStarImportMojo.java
+++ src/main/java/com/github/poliveira_fuze/AvoidStarImportMojo.java
@@ -3,8 +3,12 @@ package com.github.poliveira_fuze;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.ImportDeclaration;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.AnnotationDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.expr.ObjectCreationExpr;
+import com.github.javaparser.ast.expr.VariableDeclarationExpr;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import lombok.SneakyThrows;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
@@ -15,6 +19,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -28,55 +33,86 @@ public class AvoidStarImportMojo extends AbstractMojo {
   protected File sourceDir;
 
   @Override
-  public void execute() throws MojoExecutionException {
+  @SneakyThrows
+  public void execute() {
     try {
       Files.walk(sourceDir.toPath())
               .filter(path -> path.toString().endsWith(".java"))
-              .forEach(path -> {
-                try {
-                  expandWildcardImports(path.toFile());
-                } catch (IOException e) {
-                  getLog().error("Error processing file: " + path, e);
-                }
-              });
+              .forEach(path -> expandWildcardImports(path.toFile()));
     } catch (IOException e) {
       throw new MojoExecutionException("Error reading source directory", e);
     }
   }
 
-  private void expandWildcardImports(File file) throws IOException {
-    CompilationUnit compilationUnit;
+  @SneakyThrows
+  private CompilationUnit getCompilationUnit(File file) {
     try (FileInputStream in = new FileInputStream(file)) {
-      compilationUnit = StaticJavaParser.parse(in);
+      return StaticJavaParser.parse(in);
     }
+  }
+
+  @SneakyThrows
+  private void expandWildcardImports(File file) {
+    CompilationUnit compilationUnit = getCompilationUnit(file);
 
     Set<String> usedTypes = new HashSet<>();
     compilationUnit.accept(new VoidVisitorAdapter<Void>() {
       @Override
-      public void visit(ClassOrInterfaceDeclaration type, Void arg) {
-        usedTypes.add(type.getNameAsString());
-        super.visit(type, arg);
+      public void visit(AnnotationDeclaration annotationDeclaration, Void arg) {
+        usedTypes.add(annotationDeclaration.getNameAsString());
+        super.visit(annotationDeclaration, arg);
+      }
+
+      @Override
+      public void visit(FieldDeclaration field, Void arg) {
+        field.getVariables().forEach(var -> usedTypes.add(var.getType().asString()));
+        super.visit(field, arg);
+      }
+
+      @Override
+      public void visit(ObjectCreationExpr objCreation, Void arg) {
+        usedTypes.add(objCreation.getType().asString());
+        super.visit(objCreation, arg);
+      }
+
+      @Override
+      public void visit(VariableDeclarationExpr varDeclaration, Void arg) {
+        varDeclaration.getVariables().forEach(var -> usedTypes.add(var.getType().asString()));
+        super.visit(varDeclaration, arg);
       }
     }, null);
 
+    List<Class<?>> classes = new ArrayList<>(usedTypes.size());
+    List<ImportDeclaration> importsToRemove = new ArrayList<>();
     List<ImportDeclaration> imports = compilationUnit.getImports();
     for (ImportDeclaration imp : imports) {
       if (imp.isAsterisk()) {
-        String packageName = imp.getNameAsString();
+        importsToRemove.add(imp);
+      }
+    }
 
-        for (String className : usedTypes) {
-          try {
-            Class<?> clazz = Class.forName(packageName + "." + className);
-            compilationUnit.addImport(clazz);
-          } catch (ClassNotFoundException e) {
-            getLog().warn("Class not found: " + packageName + "." + className);
-          }
+    for (ImportDeclaration imp : importsToRemove) {
+      String packageName = imp.getNameAsString();
+      compilationUnit.remove(imp);
+      for (String className : usedTypes) {
+        try {
+          String name = packageName + "." + className.replaceAll("\\s*<.*>\\s*", "");
+          classes.add(Class.forName(name));
+        } catch (ClassNotFoundException e) {
+          getLog().warn("Class not found: " + packageName + "." + className);
         }
       }
     }
 
+    for (Class<?> clazz : classes) {
+      compilationUnit.addImport(clazz);
+    }
+
     try (FileOutputStream out = new FileOutputStream(file)) {
       out.write(compilationUnit.toString().getBytes());
+      out.flush();
+    } catch (IOException e) {
+      throw new IOException("Error writing to file: " + file, e);
     }
 
     getLog().info("Expanded wildcard imports in " + file);
